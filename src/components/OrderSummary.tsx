4f9f15f6f1b2e6011841940a765fce0cbdcc13f0
kimywann <kimywan10@gmail.com>
2025-12-04 22:42:59 +0900

    style: OrderSummary 버튼, cursor-pointer 속성 부여

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -23,11 +23,11 @@ export function OrderSummary({
 
   return (
     <section className={`flex w-full flex-col gap-4 ${className}`}>
-      <div className="m-4 flex justify-between">
+      <div className="flex justify-between">
         <p className="text-lg font-bold">총 상품 금액</p>
         <p className="text-lg font-bold">{totalAmount.toLocaleString()}원</p>
       </div>
-      <Button className="mt-4" onClick={onAction}>
+      <Button className="mt-4 cursor-pointer" onClick={onAction}>
         {buttonText}
       </Button>
     </section>
