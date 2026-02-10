676bb9ff3a9f726df101923763b0e13c90e1393e
kimywann <kimywan10@gmail.com>
2026-02-10 16:01:57 +0900

    feat: 상품 정보 없음 Not-found 페이지 구현

---
+++ src/app/(user)/product/[id]/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="container mx-auto px-4 py-20">
+      <p className="text-center text-lg font-semibold">
+        상품을 찾을 수 없습니다.
+      </p>
+    </div>
+  );
+}
+
